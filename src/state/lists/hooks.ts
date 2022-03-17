67f7fe6e9e430a6b41f1dad955c09f06a5838358
Lint Action <lint-action@samuelmeuli.com>
2022-03-17 20:48:43 +0000

    Fix code style issues with ESLint

--- src/state/lists/hooks.ts
+++ src/state/lists/hooks.ts
@@ -144,7 +144,7 @@ export function useInactiveListUrls(): string[] {
 export function useCombinedActiveList(): TokenAddressMap {
   const activeListUrls = useActiveListUrls()
   const activeTokens = useCombinedTokenMapFromUrls(activeListUrls)
-  
+
   const defaultTokenMap = listToTokenMap(DEFAULT_TOKEN_LIST)
   return combineMaps(activeTokens, defaultTokenMap)
 }
